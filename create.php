dff55b80780e3e9eba1942c7f6169a61be3b4014
6Bf <68494522+6Bf@users.noreply.github.com>
2020-11-05 11:26:05 +0900

    Update create.php

--- create.php
+++ create.php
@@ -1,18 +1,18 @@
 <!doctype html>
 <html>
 	<head>
-    	<meta charset="utf-8">
-    	<title>WEB</title>
+    		<meta charset="utf-8">
+    		<title>WEB</title>
 	</head>
 	<body>
-    	<h1>WEB</h1>
-    	<ol>
-    		<li>HTML</li>
-    	</ol>
-    	<form action="process_create.php" method="POST">
-    		<div><input type="text" name="title" placeholder="title"></div><br/>
+    		<h1>WEB</h1>
+    		<ol>
+    			<li>HTML</li>
+    		</ol>
+    		<form action="process_create.php" method="POST">
+    			<div><input type="text" name="title" placeholder="title"></div><br/>
 			<div><textarea name="description" placeholder="description"></textarea></div><br/>
-      		<div><input type="submit"></div>
-    	</form>
+      			<div><input type="submit"></div>
+    		</form>
 	</body>
 </html>
